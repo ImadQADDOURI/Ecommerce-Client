11f9246552f90a19607b229adb824e656a953697
ImadQADDOURI <imad.qaddouri@ump.ac.ma>
2023-04-05 05:48:51 +0000

    added MUI

--- src/components/header/Navbaar.js
+++ src/components/header/Navbaar.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './navbar.css';
-
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
 
 function Navbaar() {
   return (
-    <div >Navbaar</div>
+    <header>
+      <nav>
+<div className='left'>
+
+<div className='navlogo'>
+<img src="Amazon_logo.png" alt="" />
+</div>
+<AccessAlarmIcon className='bg-slate-50'/>
+</div>
+
+      </nav>
+
+
+
+    </header>
   )
 }
 
